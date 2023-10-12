8c9eff92188c0dea685fd7ec85505f79f7b97097
fipachu <80906036+fipachu@users.noreply.github.com>
2023-10-12 14:06:51 +0200

    Reintroduce length check to stage 3

--- Password Checker/task/game.py
+++ Password Checker/task/game.py
@@ -1,5 +1,16 @@
 from hashlib import sha1
 
-password = bytes(input("Enter your password: "), 'utf-8')
-hashed_password = sha1(password).hexdigest()
-print(f'Your hashed password is: {hashed_password}')
+
+def is_valid(s, min_length) -> bool:
+    return len(s) >= min_length
+
+
+while True:
+    password = bytes(input("Enter your password: "), 'utf-8')
+    if is_valid(password, 8):
+        hashed_password = sha1(password).hexdigest()
+        print(f'Your hashed password is: {hashed_password}')
+        break
+    else:
+        print('Your password is too short. Please enter a password of '
+              'at least 8 characters.')
