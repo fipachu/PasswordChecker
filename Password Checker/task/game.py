f62bb482eae80e8c5bc27e610924bb62bf56d1c4
fipachu <80906036+fipachu@users.noreply.github.com>
2023-10-13 22:46:43 +0200

    Solve stage 5/6

--- Password Checker/task/game.py
+++ Password Checker/task/game.py
@@ -1,6 +1,6 @@
-import requests
 from hashlib import sha1
 
+import requests
 
 
 def get_password(min_length=8) -> str:
@@ -12,14 +12,41 @@ def get_password(min_length=8) -> str:
 
 def main():
     password = get_password()
+    # hashlib.sha1.hexdigest() returns lowercase hex number
     hashed = sha1(password.encode('utf-8')).hexdigest()
     print(f'Your hashed password is: {hashed}')
 
-    # flush to make sure it prints before requests.get does its job
-    print('Checking...', flush=True)
-    r = requests.get(f'https://api.pwnedpasswords.com/range/{hashed[:5]}')
-    print(f'A request was sent to "{r.url}" endpoint, awaiting response...')
-    # print(r.text)  # Not yet, not in this stage
+    prefix = hashed[:5]
+    suffix = hashed[5:]
+    url = f'https://api.pwnedpasswords.com/range/{prefix}'
+    headers = {'Add-Padding': 'true'}
+
+    print('Checking...')
+    print(f'A request was sent to "{url}" endpoint, awaiting response...')
+    r = requests.get(url, headers=headers)
+    r.raise_for_status()  # Raise exception if GET failed
+
+    # Make response text lowercase for compatibility with hexdigest.
+    # ...yes it was a sneaky bug at one point...
+    # It could be the other way around, but I like the look of lowercase.
+    suffixes: str = r.text.lower()
+    suffixes: dict[str, str] = dict(line.split(':')
+                                    for line in suffixes.split('\r\n'))
+
+    if suffix in suffixes and (n := suffixes[suffix]) != 0:
+        print(f'Your password has been pwned! The password "{password}"'
+              f' appears {n} times in data breaches.')
+    else:
+        print("Good news! Your password hasn't been pwned.")
+
+    # # Alternatively, and, I think, less readably:
+    # try:
+    #     n = suffixes[suffix]
+    # except KeyError:
+    #     print("Good news! Your password hasn't been pwned.")
+    # else:
+    #     print(f'Your password has been pwned! The password "{password}"'
+    #           f' appears {n} times in data breaches.')
 
 
 # Necessary to let the unit test know to use game.py as entry point
