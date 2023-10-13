8991e5bdac66fb8269f7bb75b25de5450bc02562
fipachu <80906036+fipachu@users.noreply.github.com>
2023-10-13 19:40:45 +0200

    Refactor stage 4
    I just like the idea behind get_password() way too much. Sure it isn't
    my idea, but it looks sooo goood!

--- Password Checker/task/game.py
+++ Password Checker/task/game.py
@@ -2,26 +2,23 @@ import requests
 from hashlib import sha1
 
 
-def is_valid(s, min_length) -> bool:
-    return len(s) >= min_length
+def get_password() -> str:
+    while len(password := input('Enter your password: ')) < 8:
+        print('Your password is too short. Please enter a password of '
+              'at least 8 characters.')
+    return password
 
 
 def main():
-    while True:
-        password = bytes(input("Enter your password: "), 'utf-8')
-        if is_valid(password, 8):
-            hashed_password = sha1(password).hexdigest()
-            print(f'Your hashed password is: {hashed_password}')
+    password = get_password()
+    hashed = sha1(password.encode('utf-8')).hexdigest()
+    print(f'Your hashed password is: {hashed}')
 
-            # flush to make sure it prints before requests.get does its job
-            print('Checking...', flush=True)
-            r = requests.get(f'https://api.pwnedpasswords.com/range/{hashed_password[:5]}')
-            print(f'A request was sent to "{r.url}" endpoint, awaiting response...')
-            # print(r.text)  # Not yet, not in this stage
-            break
-        else:
-            print('Your password is too short. Please enter a password of '
-                  'at least 8 characters.')
+    # flush to make sure it prints before requests.get does its job
+    print('Checking...', flush=True)
+    r = requests.get(f'https://api.pwnedpasswords.com/range/{hashed[:5]}')
+    print(f'A request was sent to "{r.url}" endpoint, awaiting response...')
+    # print(r.text)  # Not yet, not in this stage
 
 
 # Necessary to let the unit test know to use game.py as entry point
